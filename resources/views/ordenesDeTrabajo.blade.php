b910d4a86b74f3e1eb5233cfb87031cb2f85e44f
zeqelandia <enzzio2@hotmail.com>
2021-11-07 22:05:14 -0300

    Se actualizó la vista de órdenes de trabajo para que utilice el mismo estilo que las demás vistas

--- resources/views/ordenesDeTrabajo.blade.php
+++ resources/views/ordenesDeTrabajo.blade.php
@@ -10,7 +10,7 @@
             <!-- Start: header -->
             <article>
                 <div class="row">
-                    <div class="col" style="width: 20px;max-width: 65px;"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" href="reparaciones" title="Volver a reparaciones"><i class="fa fa-arrow-left"></i></a></div>
+                    <div class="col" style="width: 20px;max-width: 65px;"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" href="reparaciones.html" title="Volver a reparaciones"><i class="fa fa-arrow-left"></i></a></div>
                     <div class="col d-flex justify-content-xl-center align-items-xl-center">
                         <h3 class="text-center" style="font-size: 17px;margin-top: 0;font-weight: 700;margin-bottom: 0;">Usted ha seleccionado la siguiente reparación</h3>
                     </div>
@@ -71,72 +71,58 @@
 </div>
 <!-- End: #reparacion -->
 <!-- Start: #ordenDeTrabajo -->
-<div class="card shadow" style="margin-top: 15px;">
-    <div class="card-body">
-        <div class="row">
-            <div class="col" style="max-width: 200px;">
-                <h4 style="font-weight: 700;font-size: 17px;">Orden de trabajo 1</h4>
+<div class="card shadow" style="margin-top: 30px;">
+    <div class="card-header align-items-center">
+        <div class="row" style="width: 100%;">
+            <div class="col d-xl-flex align-items-xl-center">
+                <h6 class="text-primary d-xl-flex align-items-xl-center font-weight-bold m-0">Orden de trabajo 1</h6>
             </div>
+            <div class="col d-xl-flex justify-content-xl-end"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" href="confirmarOrdenDeTrabajo.html" title="Confirmar esta orden de trabajo" style="background-color: rgb(78,223,84);"><i class="fa fa-check"></i></a>
+                <button
+                    class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,87);" title="Cancelar esta orden de trabajo"><i class="fa fa-remove"></i></button>
+            </div>
+        </div>
+        <div class="row" style="width: 100%;">
             <div class="col">
-                <div class="row" style="margin-right: 0;">
-                    <div class="col" style="padding-right: 11px;">
-                        <p style="margin-bottom: 0;font-size: 13px;">Orden de trabajo</p>
-                    </div>
-                </div>
                 <div class="row">
-                    <div class="col"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" href="confirmarOrdenDeTrabajo.html" title="Confirmar esta orden de trabajo" style="background-color: rgb(78,223,84);"><i class="fa fa-check"></i></a>
-                        <button
-                            class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,87);" title="Cancelar esta orden de trabajo"><i class="fa fa-remove"></i></button>
+                    <div class="col" style="width: 60px;max-width: 60px;min-width: 60px;">
+                        <p style="margin-bottom: 0;font-weight: 700;width: 60px;">Estado:</p>
+                    </div>
+                    <div class="col" style="width: 110px;min-width: 110px;max-width: 110px;">
+                        <p style="margin-bottom: 0;width: 90px;">En proceso</p>
+                    </div>
+                    <div class="col" style="width: 110px;max-width: 110px;">
+                        <p style="margin-bottom: 0;font-weight: 700;width: 100px;">% de avance:</p>
+                    </div>
+                    <div class="col" style="width: 50px;max-width: 50px;">
+                        <p style="margin-bottom: 0;width: 50px;">0%</p>
+                    </div>
+                    <div class="col" style="max-width: 120px;">
+                        <p style="margin-bottom: 0;font-weight: 700;width: 120px;">Horas totales:</p>
+                    </div>
+                    <div class="col">
+                        <p style="margin-bottom: 0;width: 45px;">2H</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="row">
-                    <div class="col">
-                        <p style="margin-bottom: 0;font-weight: 700;">Estado:</p>
-                    </div>
-                    <div class="col">
-                        <p style="margin-bottom: 0;">En proceso</p>
-                    </div>
-                    <div class="col">
-                        <p style="margin-bottom: 0;font-weight: 700;">% de avance:</p>
-                    </div>
-                    <div class="col">
-                        <p style="margin-bottom: 0;">0%</p>
-                    </div>
-                    <div class="col">
-                        <p style="margin-bottom: 0;font-weight: 700;">Horas totales:</p>
-                    </div>
-                    <div class="col">
-                        <p style="margin-bottom: 0;">2H</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+    <div class="card-body" style="padding-top: 0;">
         <div class="card" style="margin-top: 15px;">
-            <div class="card-body">
+            <div class="card-header align-items-center">
+                <div class="row" style="width: 100%;">
+                    <div class="col d-xl-flex align-items-xl-center">
+                        <h6 class="text-primary d-xl-flex align-items-xl-center font-weight-bold m-0">Tarea 1</h6>
+                    </div>
+                    <div class="col d-xl-flex justify-content-xl-end"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" style="background-color: rgb(81,223,78);" href="completarTarea.html" title="Completar esta tarea"><i class="fa fa-check"></i></a>
+                        <button
+                            class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,95);" title="Eliminar esta tarea"><i class="fa fa-remove"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body" style="padding-top: 0;padding-bottom: 0;">
                 <!-- Start: #tarea -->
                 <section>
-                    <div class="row" style="margin-top: 15px;">
-                        <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" style="width: 100px;min-width: 10px;max-width: 100px;">
-                            <h4 style="font-weight: 700;font-size: 17px;">Tarea 1</h4>
-                        </div>
-                        <div class="col">
-                            <div class="row">
-                                <div class="col">
-                                    <p style="font-size: 13px;margin-bottom: 0;">Tarea</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" style="background-color: rgb(81,223,78);" href="completarTarea.html" title="Completar esta tarea"><i class="fa fa-check"></i></a>
-                                    <button
-                                        class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,95);" title="Eliminar esta tarea"><i class="fa fa-remove"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="row">
                         <div class="col">
                             <div class="table-responsive">
@@ -208,27 +194,20 @@
             </div>
         </div>
         <div class="card" style="margin-top: 15px;">
-            <div class="card-body">
+            <div class="card-header align-items-center">
+                <div class="row" style="width: 100%;">
+                    <div class="col d-xl-flex align-items-xl-center">
+                        <h6 class="text-primary d-xl-flex align-items-xl-center font-weight-bold m-0">Tarea 2</h6>
+                    </div>
+                    <div class="col d-xl-flex justify-content-xl-end"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" style="background-color: rgb(81,223,78);" href="completarTarea.html" title="Completar esta tarea"><i class="fa fa-check"></i></a>
+                        <button
+                            class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,95);" title="Eliminar esta tarea"><i class="fa fa-remove"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body" style="padding-top: 0;padding-bottom: 0;">
                 <!-- Start: #tarea -->
                 <section>
-                    <div class="row" style="margin-top: 15px;">
-                        <div class="col d-xl-flex justify-content-xl-center align-items-xl-center" style="width: 100px;min-width: 10px;max-width: 100px;">
-                            <h4 style="font-weight: 700;font-size: 17px;">Tarea 2</h4>
-                        </div>
-                        <div class="col">
-                            <div class="row">
-                                <div class="col">
-                                    <p style="font-size: 13px;margin-bottom: 0;">Tarea</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" style="background-color: rgb(81,223,78);" href="completarTarea.html" title="Completar esta tarea"><i class="fa fa-check"></i></a>
-                                    <button
-                                        class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;background-color: rgb(223,78,95);" title="Eliminar esta tarea"><i class="fa fa-remove"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="row">
                         <div class="col">
                             <div class="table-responsive">
