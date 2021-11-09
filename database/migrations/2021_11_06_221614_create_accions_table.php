e30ffe272dff2efaec3c35f2f5cc3d02dc87fe96
Sanntin <sanntinunez@gmail.com>
2021-11-08 23:49:32 -0300

    cambio en nombre de base de datos

--- database/migrations/2021_11_06_221614_create_accions_table.php
+++ database/migrations/2021_11_06_221614_create_accions_table.php
@@ -13,7 +13,7 @@ class CreateAccionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('acciones', function (Blueprint $table) {
+        Schema::create('accions', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->float('precio');
@@ -29,6 +29,6 @@ class CreateAccionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('acciones');
+        Schema::dropIfExists('accions');
     }
 }
