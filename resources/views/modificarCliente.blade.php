7411eed1c039e0edb25b71adf0db63837b399e7d
Sanntin <sanntinunez@gmail.com>
2021-11-26 09:53:38 -0300

    modificar cliente

--- resources/views/modificarCliente.blade.php
+++ resources/views/modificarCliente.blade.php
@@ -14,13 +14,16 @@
         <div class="col-lg-7">
             <div class="p-5">
             @if (session()->has('errorId'))
-                <p>{{session()->get('errorId')}}</p>
+                <p  class="text-red-500 text-xs mt-1">{{session()->get('errorId')}}</p>
             @endif
+                
+         
+        
                 <form  method="POST"  action="/cliente/modificar/guardar">
                     @csrf
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Nombre" name="nombre" value={{$cliente->nombre}}>
+                            <input class="form-control form-control-user" type="text" id="nombre" placeholder="Nombre" name="nombre" value={{empty($cliente->nombre) ? old('nombre'): $cliente->nombre }}>
                             @error('nombre')
                             {{-- !!! @eze formatear el texto de error --}}
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -28,7 +31,7 @@
                         </div>
 
                         <div class="col-sm-6">
-                            <input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Apellido" name="apellido" value={{$cliente->apellido}}>
+                            <input class="form-control form-control-user" type="text" id="apellido" placeholder="Apellido" name="apellido" value={{empty($cliente->apellido) ? old('apellido'): $cliente->apellido }}>
                             @error('apellido')
                             {{-- !!! @eze formatear el texto de error --}}
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -37,10 +40,10 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                        <input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="DNI" name="dni" disabled value={{$cliente->dni}}>
+                        <input class="form-control form-control-user" type="text" id="dni" placeholder="DNI" name="dni" disabled value={{empty($cliente->dni) ? old('dni'): $cliente->dni }}>
                         </div>
                         <div class="col-sm-6">
-                            <input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Teléfono" name="telefono" value={{$cliente->telefono}}>
+                            <input class="form-control form-control-user" type="text" id="telefono" placeholder="Teléfono" name="telefono" value={{empty($cliente->telefono) ? old('telefono'): $cliente->telefono }}>
                             @error('dni')
                             {{-- !!! @eze formatear el texto de error --}}
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -49,7 +52,7 @@
                     </div>
 
                     <div class="form-group">
-                        <input class="form-control form-control-user" type="email" id="email"  placeholder="Mail" name="mail" autocomplete="off" value={{$cliente->mail}}>
+                        <input class="form-control form-control-user" type="email" id="email"  placeholder="Mail" name="mail" autocomplete="off" value={{empty($cliente->mail) ? old('mail'): $cliente->mail }}>
                         @error('mail')
                         {{-- !!! @eze formatear el texto de error --}}
                         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -58,14 +61,14 @@
 
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input class="form-control form-control-user" type="text" id="localidad" placeholder="Localidad" name="localidad" autocomplete="off" value={{$cliente->localidad}}>
+                            <input class="form-control form-control-user" type="text" id="localidad" placeholder="Localidad" name="localidad" autocomplete="off" value={{empty($cliente->localidad) ? old('localidad'): $cliente->localidad }}>
                             @error('localidad')
                             {{-- !!! @eze formatear el texto de error --}}
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
                         </div>
                         <div class="col-sm-6">
-                            <input class="form-control form-control-user" type="text" id="exampleFirstName-1" placeholder="Dirección" name="direccion" value={{$cliente->direccion}}>
+                            <input class="form-control form-control-user" type="text" id="direccion" placeholder="Dirección" name="direccion" value={{empty($cliente->direccion) ? old('direccion'): $cliente->direccion }}>
                             @error('direccion')
                             {{-- !!! @eze formatear el texto de error --}}
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -74,7 +77,7 @@
                     </div>
                     <div class="form-group row">
                         <div id="btnCancelarModificarCliente" class="col-sm-6 mb-3 mb-sm-0"><button class="btn btn-primary btn-block text-white btn-user" type="button" style="background-color: rgb(223,78,104);" onclick=" window.location = '/clientes'">Cancelar</button></div>
-                        <div id="btnModificarCliente" class="col-sm-6"><button class="btn btn-primary btn-block text-white btn-user" type="submit">Guardar cambios</button></div>
+                        <div id="btnModificarClienteGuardar" class="col-sm-6"><button class="btn btn-primary btn-block text-white btn-user" type="submit">Guardar cambios</button></div>
                     </div>
                 </form>
             </div>
