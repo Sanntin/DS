188eab3295fe281350ebf0660b7b2d1974577f60
zeqelandia <enzzio2@hotmail.com>
2021-12-07 19:28:28 -0300

    Se agregó la caja de búsqueda a la vista de reparaciones

--- resources/views/reparaciones.blade.php
+++ resources/views/reparaciones.blade.php
@@ -19,6 +19,14 @@
             <h6 class="text-primary font-weight-bold m-0">Reparaciones</h6>
         </div>
         <div class="col d-xl-flex justify-content-xl-end">
+            <div class="input-group search-box">
+                <input class="bg-light form-control border-0 small" type="text" placeholder="Buscar ...">
+                <div class="input-group-append">
+                    <button class="btn btn-primary py-0" type="button">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
             <!-- Start: #generarReparacion --><a class="btn btn-primary" role="button" data-toggle="tooltip" data-bs-tooltip="" style="margin-left: 10px;background-color: rgb(116,223,78);" title="Nueva reparación" href="/generarReparacion"><i class="fa fa-plus"></i></a>
                     <!-- End: #generarReparacion -->
                     <!-- Start: #reporteReparaciones --><button id="btnGenerarReporte" class="btn btn-primary" data-toggle="tooltip" data-bs-tooltip="" type="button" style="margin-left: 10px;" title="Generar reporte de reparaciones"><i class="fa fa-list-alt"></i></button>
